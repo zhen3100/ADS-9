909ae8aa713a60d41c719c392cfff6a19d199853
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2022-05-07 12:38:18 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,25 +6,25 @@
 BST<std::string> makeTree(const char* filename);
 
 TEST(lab6, test1_1) {
-    const char* filename = "/home/runner/work/ADS-6/ADS-6/build/test/war_peace.txt";
+    const char* filename = "/home/runner/work/ADS-9/ADS-9/build/test/war_peace.txt";
     BST<std::string> tree = makeTree(filename);
     int depth = tree.depth();
     EXPECT_EQ(depth, 35);
 }
 TEST(lab6, test1_2) {
-    const char* filename = "/home/runner/work/ADS-6/ADS-6/build/test/war_peace.txt";
+    const char* filename = "/home/runner/work/ADS-9/ADS-9/build/test/war_peace.txt";
     BST<std::string> tree = makeTree(filename);
     int depth = tree.search("pierre");
     EXPECT_EQ(depth, 1963);
 }
 TEST(lab6, test1_3) {
-    const char* filename = "/home/runner/work/ADS-6/ADS-6/build/test/war_peace.txt";
+    const char* filename = "/home/runner/work/ADS-9/ADS-9/build/test/war_peace.txt";
     BST<std::string> tree = makeTree(filename);
     int depth = tree.search("natasha");
     EXPECT_EQ(depth, 1212);
 }
 TEST(lab6, test1_4) {
-    const char* filename = "/home/runner/work/ADS-6/ADS-6/build/test/war_peace.txt";
+    const char* filename = "/home/runner/work/ADS-9/ADS-9/build/test/war_peace.txt";
     BST<std::string> tree = makeTree(filename);
     int depth = tree.search("andrew");
     EXPECT_EQ(depth, 1143);
