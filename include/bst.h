8f6e46aedcba70c98a67adc20cb129b0efcbe61b
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-05-26 23:45:37 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -19,8 +19,8 @@ class BST {
     }
     void add(T value) {
         root = addNode(root, value);
-    } 
- private:
+    }
+  private:
     Node* root;
     Node* addNode(Node* root, T value) {
         if (root == nullptr) {
