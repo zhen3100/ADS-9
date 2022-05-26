e48809b292e3ade4fd670697a9424ac533a946e1
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-05-26 23:32:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,26 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+    BST<std::string> Tree;
+    std::string symb = "";
+    // если файл не открылся, прекращаем работу
+    if (!file) {
+        throw std::string("File not found");
+    }
+    // читаем содержимое файла посимвольно
+    while (!file.eof()) {
+        symb = "";
+        while (true) {
+            char c = file.get();
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                symb += tolower(c);
+            else
+                break;
+        }
+        Tree.add(symb);
+    }
+    return Tree;
+    // закрываем файл
+    file.close();
 }
